042424c986e20ff878de371896490636b5d079be
13612989029@163.com <huanghaojie@bingosoft.net>
2019-08-19 18:50:32 +0800

    更换小部件国际化语言

--- src/js/lang.js
+++ src/js/lang.js
@@ -1,18 +1,18 @@
 module.exports = {
     "zh-cn": {
-        "new": '新闻列表',
+        "new": '新闻',
         "All": '更多',
         "NoneData": "暂无新闻",
         "ErrorLoadData": "加载失败"
     },
     "zh-hk": {
-        "new": '新聞列表',
+        "new": '新聞',
         "All": '更多',
         "NoneData": "暫無新聞",
         "ErrorLoadData": "加載失敗"
     },
     "en": {
-        "new": 'News list',
+        "new": 'News',
         "All": 'More',
         "NoneData": "No data",
         "ErrorLoadData": "loading failed"
